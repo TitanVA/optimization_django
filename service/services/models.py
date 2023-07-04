b2704f5378108a2554cb0de6d8f8870b4ded727c
Viktor <viktorbezai@gmail.com>
2023-07-04 18:38:58 -0400

    Caching with redis

--- service/services/models.py
+++ service/services/models.py
@@ -1,8 +1,10 @@
 from django.core.validators import MaxValueValidator
 from django.db import models
+from django.db.models.signals import post_delete
 
 from clients.models import Client
-from services.tasks import set_price
+from services.receivers import delete_cache_total_sum
+from services.tasks import set_price, set_comment
 
 
 class Service(models.Model):
@@ -20,6 +22,7 @@ class Service(models.Model):
         if self.full_price != self.__full_price:
             for subscription in self.subscriptions.all():
                 set_price.delay(subscription.id)
+                set_comment.delay(subscription.id)
         return super().save(*args, **kwargs)
 
 
@@ -44,6 +47,7 @@ class Plan(models.Model):
         if self.discount_percent != self.__discount_percent:
             for subscription in self.subscriptions.all():
                 set_price.delay(subscription.id)
+                set_comment.delay(subscription.id)
         return super(Plan, self).save(*args, **kwargs)
 
 
@@ -52,8 +56,17 @@ class Subscription(models.Model):
     service = models.ForeignKey(Service, related_name="subscriptions", on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name="subscriptions", on_delete=models.PROTECT)
     price = models.PositiveIntegerField(default=0)
+    comment = models.CharField(max_length=100, default="")
 
     def __str__(self):
         return f"{self.client.company_name}: {self.service.name}"
 
+    def save(self, *args, **kwargs):
+        creating = not bool(self.id)
+        result = super(Subscription, self).save(*args, **kwargs)
+        if creating:
+            set_price.delay(self.id)
+        return result
 
+
+post_delete.connect(delete_cache_total_sum, sender=Subscription)
--- service/services/views.py
+++ service/services/views.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from django.db.models import Prefetch, F, Sum
 from rest_framework.viewsets import ReadOnlyModelViewSet
-
+from django.core.cache import cache
 from clients.models import Client
 from services.models import Subscription
 from services.serializers import SubscriptionSerializer
@@ -18,8 +19,15 @@ class SubscriptionView(ReadOnlyModelViewSet):
         queryset = self.filter_queryset(self.get_queryset())
         response = super().list(request, *args, **kwargs)
 
-        response_data = {"result": response.data}
-        response_data["total_amount"] = queryset.aggregate(total=Sum("price")).get("total")
+        price_cash = cache.get(settings.PRICE_CACHE_NAME)
+
+        if price_cash:
+            total_price = price_cash
+        else:
+            total_price = queryset.aggregate(total=Sum("price")).get("total")
+            cache.set(settings.PRICE_CACHE_NAME, total_price, 60 * 60)
+
+        response_data = {"result": response.data, "total_amount": total_price}
         response.data = response_data
 
         return response
